f372a467d9709a3c59d165b069218c525f04e16c
Tanveesh Singh Chaudhery <tanveeshs@gmail.com>
2020-06-03 19:01:10 +0530

    Added header and footer

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -40,5 +39,4 @@ app.use(function(err, req, res, next) {
 
 app.listen(3000,function (err) {
   console.log('Server started')
-
 })
