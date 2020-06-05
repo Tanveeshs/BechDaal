787171dc729ca4652e33ee2439afd81c048f5f69
Tanveesh Singh Chaudhery <Tanveeshs@gmail.com>
2020-06-05 20:32:09 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	node_modules/after/package.json
    #	node_modules/arraybuffer.slice/package.json
    #	node_modules/async-limiter/package.json
    #	node_modules/backo2/package.json
    #	node_modules/base64-arraybuffer/package.json
    #	node_modules/base64id/package.json
    #	node_modules/better-assert/package.json
    #	node_modules/blob/package.json
    #	node_modules/callsite/package.json
    #	node_modules/component-bind/package.json
    #	node_modules/component-emitter/package.json
    #	node_modules/component-inherit/package.json
    #	node_modules/engine.io-client/node_modules/component-emitter/package.json
    #	node_modules/engine.io-client/node_modules/debug/package.json
    #	node_modules/engine.io-client/node_modules/ms/package.json
    #	node_modules/engine.io-client/node_modules/ws/package.json
    #	node_modules/engine.io-client/package.json
    #	node_modules/engine.io-parser/package.json
    #	node_modules/engine.io/node_modules/cookie/package.json
    #	node_modules/engine.io/node_modules/debug/package.json
    #	node_modules/engine.io/node_modules/ms/package.json
    #	node_modules/engine.io/package.json
    #	node_modules/express-session/node_modules/depd/package.json
    #	node_modules/express-session/node_modules/safe-buffer/package.json
    #	node_modules/express-session/package.json
    #	node_modules/has-binary2/package.json
    #	node_modules/has-cors/package.json
    #	node_modules/indexof/package.json
    #	node_modules/isarray/package.json
    #	node_modules/object-component/package.json
    #	node_modules/parseqs/package.json
    #	node_modules/parseuri/package.json
    #	node_modules/random-bytes/package.json
    #	node_modules/socket.io-adapter/package.json
    #	node_modules/socket.io-client/node_modules/debug/package.json
    #	node_modules/socket.io-client/node_modules/ms/package.json
    #	node_modules/socket.io-client/node_modules/socket.io-parser/node_modules/debug/package.json
    #	node_modules/socket.io-client/node_modules/socket.io-parser/node_modules/ms/package.json
    #	node_modules/socket.io-client/node_modules/socket.io-parser/package.json
    #	node_modules/socket.io-client/package.json
    #	node_modules/socket.io-parser/node_modules/debug/package.json
    #	node_modules/socket.io-parser/node_modules/ms/package.json
    #	node_modules/socket.io-parser/package.json
    #	node_modules/socket.io/node_modules/debug/package.json
    #	node_modules/socket.io/node_modules/ms/package.json
    #	node_modules/socket.io/package.json
    #	node_modules/to-array/package.json
    #	node_modules/uid-safe/package.json
    #	node_modules/ws/package.json
    #	node_modules/xmlhttprequest-ssl/package.json
    #	node_modules/yeast/package.json

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'googleAuth': {
+    'clientID': '934010560730-jefj4ol40kglpgb62f87ob87mivq6ani.apps.googleusercontent.com',
+    'clientSecret': 'kdEGBP_Gay8s_XpK3hqNf9mG',
+    'callbackURL': 'http://localhost:3000/auth/google/callback'
+  }
+
+
+};
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // 'url': process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL || '172.30.118.50:27017'
+  'url': 'mongodb+srv://admin:admin@cluster0-pavbm.mongodb.net/Bechdaal?retryWrites=true&w=majority'
+};
