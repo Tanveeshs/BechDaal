c25241843d1d19efa2302d2b78f186bf8e7efe4c
Tanveesh Singh Chaudhery <tanveeshs@gmail.com>
2020-06-06 16:54:18 +0530

    Forgot Password added

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const flash = require('connect-flash');
 const morgan = require('morgan');
 const session = require('express-session');
 const configDB = require('./config/database');
+const forgotPass = require('./routes/forgotPass');
 mongoose.connect(configDB.url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -33,6 +34,12 @@ app.use(morgan('dev'));
 app.use(session({
   secret: 'XYZZ'
 }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
@@ -41,6 +48,9 @@ require('./routes/RegisterAndLogin')(app, passport);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/user', forgotPass);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ utils/mailer.js
@@ -0,0 +1,28 @@
+//jshint esversion:6
+
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user:'bechdaal1@gmail.com',
+    pass:'oherodyprkvomjwo'
+  }
+});
+module.exports = function(email, content) {
+  let mail = {
+    from: 'indentinternship@gmail.com',
+    to: email,
+    subject: 'Test Mail',
+    text: content
+  };
+  transporter.sendMail(mail, function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+      console.log('Mail sent successfully');
+    }
+
+  });
+
+};
