f59cb49f578590bd403636c02e752a4f66ef9a1b
Rish-P <ar.pee@aol.com>
2020-06-09 12:32:27 +0530

    added all input cards and the button, only images left to be added

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(morgan('dev'));
 app.use(session({
   secret: 'XYZZ'
 }));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -55,12 +55,12 @@ app.use('/user', forgotPass);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -70,6 +70,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000,function (err) {
+app.listen(3000, function (err) {
   console.log('Server started');
 });
