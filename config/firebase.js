66a60be4acbfdf568535edee901e757c03688c69
Tanveesh Singh Chaudhery <Tanveeshs@gmail.com>
2020-06-18 02:05:59 +0530

    CHAT

---
+++ config/firebase.js
@@ -0,0 +1,9 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require('../chatapp-ced19-firebase-adminsdk-hfa4g-300e5a9f9d.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://chatapp-ced19.firebaseio.com"
+});
+module.exports = admin
