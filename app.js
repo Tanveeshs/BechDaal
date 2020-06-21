9141838923f0f847890d9e38b23543ee4e6f7d39
Tanveesh Singh Chaudhery <Tanveeshs@gmail.com>
2020-06-21 17:40:32 +0530

    Revert "removed jshint errors"
    This reverts commit 2102fc9a

--- app.js
+++ app.js
@@ -3,8 +3,6 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const flash = require('connect-flash');
@@ -52,8 +50,6 @@ app.get('/show_ad', (req, res) => {
   });
 });
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/user', forgotPass);
 app.use('/verify', verifymail);
 app.use('/sell', adRoute);
--- routes/RegisterAndLogin.js
+++ routes/RegisterAndLogin.js
@@ -1,21 +1,13 @@
 //jshint esversion:6
 
-<<<<<<< HEAD
-module.exports = function (app, passport) {
-    app.get('/', function (req, res) {
-        res.render('index.ejs', { user: undefined });
-=======
+
 module.exports = function(app, passport) {
-  app.get('/', function(req, res) {
-    res.render('index.ejs', {
-      user: undefined
->>>>>>> 2102fc9a04c79cdf40b5a536ad140fc18e14f6fa
+    app.get('/', function (req, res) {
+        res.render('index.ejs', {
+            user: undefined
+        });
     });
-});
-
-app.get('/login', function (req, res) {
     app.get('/login', function (req, res) {
-
         // render the page and pass in any flash data if it exists
         res.render('login.ejs', {
             message: req.flash('loginMessage')
@@ -28,112 +20,65 @@ app.get('/login', function (req, res) {
         failureFlash: true // allow flash messages
     }));
 
-<<<<<<< HEAD
     app.get('/signup', function (req, res) {
 
         // render the page and pass in any flash data if it exists
         res.render('signup.ejs', {
             message: req.flash('signupMessage')
         });
-        console.log(req.body);
-=======
-  app.get('/signup', function(req, res) {
-
-    // render the page and pass in any flash data if it exists
-    res.render('signup.ejs', {
-      message: req.flash('signupMessage')
->>>>>>> 2102fc9a04c79cdf40b5a536ad140fc18e14f6fa
     });
-});
 
-app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/verify', // redirect to the secure profile section
-    failureRedirect: '/signup', // redirect back to the signup page if there is an error
-    failureFlash: true // allow flash messages
-}));
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/verify', // redirect to the secure profile section
+        failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
 
-<<<<<<< HEAD
-app.get('/verify', function (req, res) {
-    res.render('verify.ejs', {
-        user: req.user // get the user out of session and pass to template
+    app.get('/verify', function (req, res) {
+        res.render('verify.ejs', {
+            user: req.user // get the user out of session and pass to template
+        });
     });
-=======
-  app.get('/verify', function(req, res) {
-    res.render('verify.ejs', {
-      user: req.user // get the user out of session and pass to template
->>>>>>> 2102fc9a04c79cdf40b5a536ad140fc18e14f6fa
-});
-  });
 
-<<<<<<< HEAD
-app.get('/profile', isLoggedIn, function (req, res) {
-    res.render('profile.ejs', {
-        user: req.user // get the user out of session and pass to template
+    app.get('/profile', isLoggedIn, function (req, res) {
+        res.render('profile.ejs', {
+            user: req.user // get the user out of session and pass to template
+        });
+        console.log(req);
     });
-    console.log(req)
-=======
-  app.get('/profile', isLoggedIn, function(req, res) {
-    res.render('profile.ejs', {
-      user: req.user // get the user out of session and pass to template
->>>>>>> 2102fc9a04c79cdf40b5a536ad140fc18e14f6fa
-});
-console.log(req);
-  });
 
 
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+    //
+    // app.get('/auth/facebook', passport.authenticate('facebook', {
+    //     scope: ['email']
+    // }));
+    // app.get('/auth/facebook/callback',
+    //     passport.authenticate('facebook', {
+    //         successRedirect: '/profile',
+    //         failureRedirect: '/'
+    //     }));\
 
-<<<<<<< HEAD
-app.get('/logout', function (req, res) {
-    req.logout();
-    res.redirect('/');
-});
-//
-// app.get('/auth/facebook', passport.authenticate('facebook', {
-//     scope: ['email']
-// }));
-// app.get('/auth/facebook/callback',
-//     passport.authenticate('facebook', {
-//         successRedirect: '/profile',
-//         failureRedirect: '/'
-//     }));\
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-=======
-  app.get('/logout', function(req, res) {
-    req.logout();
-    res.redirect('/');
-  });
-  //
-  // app.get('/auth/facebook', passport.authenticate('facebook', {
-  //     scope: ['email']
-  // }));
-  // app.get('/auth/facebook/callback',
-  //     passport.authenticate('facebook', {
-  //         successRedirect: '/profile',
-  //         failureRedirect: '/'
-  //     }));\
+    // the callback after google has authenticated the user
+    app.get('/auth/google/callback',
+        passport.authenticate('google', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        }));
 
-  app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-  }));
+}
 
-  // the callback after google has authenticated the user
-  app.get('/auth/google/callback',
-    passport.authenticate('google', {
-      successRedirect: '/profile',
-      failureRedirect: '/'
->>>>>>> 2102fc9a04c79cdf40b5a536ad140fc18e14f6fa
-}));
-
-
-
-};
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
-
     res.redirect('/');
-
 }
+
