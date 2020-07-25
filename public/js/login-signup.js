9a7d64835761b394389a0eee9f2a2e1539abb391
Vivek Vinze <vivek.vinze@gmail.com>
2020-07-25 22:09:18 +0530

    Added popover requirements

--- public/js/login-signup.js
+++ public/js/login-signup.js
@@ -1,5 +1,10 @@
 //jshint esversion:6
 
+$(function () {
+    // alert("Hi");
+  $('[data-toggle="popover"]').popover()
+});
+
 function isEmail(email) {
   var regex1 = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
   return regex1.test(email);
