8f3d5d805412d6a22007b099a12e476ca295c743
Nishant Dave <davenishant2k@gmail.com>
2020-08-03 16:55:14 +0530

    changed offers main route

--- routes/offers.js
+++ routes/offers.js
@@ -27,40 +27,11 @@ router.post('/:adId',function(req,res){
   res.redirect('/')
 });
 
-router.get('/:userId',function(req, res){
-    const _id = req.params.userId;
-    User.findById(_id)
+router.get('/',function(req, res){
+    User.findById(req.user._id)
         .then((user) => {
             if (user != null) {
                 res.send(user.offers);
-        //       User.findById(req.user._id)
-        //         .then((user) => {
-        //           if (user.wishlist.includes(_id)) {
-        //             user.wishlist.splice(user.wishlist.indexOf(_id), 1);
-        //           } else {
-        //             user.wishlist.push(_id);
-        //           }
-        //           user.save()
-        //             .then((user) => {
-        //               res.statusCode = 200;
-        //               res.setHeader('Content-Type', 'application/json');
-        //               res.render('mywishlist', {
-        //                 user: req.user,
-        //                 array: ad
-        //               });
-        //               // res.redirect('/wish')
-        //             });
-        //         });
-        //     } else {
-        //       console.log('the ad id is wrong!');
-        //       res.statusCode = 404;
-        //       res.setHeader('Content-Type', 'application/json');
-        //       res.json({
-        //         msg: 'wrong ad id'
-        //       });
-        //     }
-        //   })
-        //   .catch((err) => console.log(err));
         }
     });
 });
