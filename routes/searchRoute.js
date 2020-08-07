c1063018c70e2287c64aa3e67de8926c8db94562
Rishabh Nevatia <nevatia.rishabh@gmail.com>
2020-08-07 13:49:58 +0530

    Main search route created for searches on homepage to be displayed

--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -27,6 +27,80 @@ searchRouter.use(bodyParser.json());
 //     res.json(data);
 //   }).limit(5);
 // });
+searchRouter.post('/main', function(req, res, next) {
+  //console.log(req.body);
+  var q = req.body.searchInput;
+  const regexp1 = new RegExp(`^${q}`, 'i'); // for finding it as beginning of the first word
+  const regexp2 = new RegExp(`\\s${q}`, 'i'); // for finding as beginning of a middle word
+  flag = 0;
+  let res1 = {};
+
+  try {
+       //Step 1: declare promise
+
+       var myPromise = () => {
+         return new Promise((resolve, reject) => {
+           flag = 0;
+           if (flag == 0) {
+             Ads.find({
+               sub_category: {
+                 $in: [regexp1, regexp2]
+               }
+             }, function(err, data) {
+               if (data.length != 0) {
+                 res1 = {
+                   Route: 1,
+                   data: data
+                 };
+
+                 flag = 1;
+
+               }
+               err
+                  ? reject(err)
+                  : resolve(res1);
+             }).limit(5);
+           }
+           if (flag != 1) {
+             flag = 0;
+             Ads.find({
+               brand: {
+                 $in: [regexp1, regexp2]
+               }
+             }, function(err, data) {
+               if (data.length != 0) {
+                 res1 = {
+                   Route: 2,
+                   data: data
+                 };
+               }
+               err
+                  ? reject(err)
+                  : resolve(res1);
+             }).limit(5);
+           }
+
+
+         });
+       };
+
+       //Step 2: async promise handler
+       var callMyPromise = async () => {
+
+          var result = await (myPromise());
+          //anything here is executed after result is resolved
+          return result;
+       };
+
+       //Step 3: make the call
+       callMyPromise().then(function(result) {
+          res.json(result);
+       });
+
+   } catch (e) {
+     next(e)
+   }
+});
 
 searchRouter.post('/search/:q', function(req, res, next) {
   var q = req.params.q;
@@ -94,7 +168,6 @@ searchRouter.post('/search/:q', function(req, res, next) {
 
        //Step 3: make the call
        callMyPromise().then(function(result) {
-           console.log(result)
           res.json(result);
        });
 
