047694b9e0df51e1908f491120275ce9dcb16be0
Nishant Dave <davenishant2k@gmail.com>
2020-08-07 17:01:23 +0530

    Changes in searchroute

--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -32,74 +32,103 @@ searchRouter.post('/main', function(req, res, next) {
   var q = req.body.searchInput;
   const regexp1 = new RegExp(`^${q}`, 'i'); // for finding it as beginning of the first word
   const regexp2 = new RegExp(`\\s${q}`, 'i'); // for finding as beginning of a middle word
-  flag = 0;
-  let res1 = {};
+  var res1 = {
+    data: [],
+  };
 
   try {
-       //Step 1: declare promise
+    //Step 1: declare promise
 
-       var myPromise = () => {
-         return new Promise((resolve, reject) => {
-           flag = 0;
-           if (flag == 0) {
-             Ads.find({
-               sub_category: {
-                 $in: [regexp1, regexp2]
-               }
-             }, function(err, data) {
-               if (data.length != 0) {
-                 res1 = {
-                   Route: 1,
-                   data: data
-                 };
+    var myPromise = () => {
+      return new Promise((resolve, reject) => {
+//find in subcat
+        Ads.find({
+          sub_category: {
+            $in: [regexp1, regexp2]
+          }
+        }, function(err, data) {
+          if (data.length != 0) {
+            data.forEach(item => {
+              res1.data.push(item);
+            });
+          }
+        });
+    //find in title
+    Ads.find({
+      title: {
+        $in: [regexp1, regexp2]
+      }
+    }, function(err, data) {
+      if (data.length != 0) {
+        data.forEach(item => {
+          res1.data.push(item);
+        });
+      }
+    });
 
-                 flag = 1;
+    //find in brand
+            Ads.find({
+              brand: {
+                $in: [regexp1, regexp2]
+              }
+            }, function(err, data) {
+              if (data.length != 0) {
+                data.forEach(item => {
+                  res1.data.push(item);
+                });
+              }
+            });
+            
+    //find in category
+    Ads.find({
+      category: {
+        $in: [regexp1, regexp2]
+      }
+    }, function(err, data) {
+      if (data.length != 0) {
+        data.forEach(item => {
+          res1.data.push(item);
+        });
+      }
+    });
+    //find in description
+    Ads.find({
+      description: {
+        $in: [regexp1, regexp2]
+      }
+    }, function(err, data) {
+      if (data.length != 0) {
+        data.forEach(item => {
+          res1.data.push(item);
+        });
+      }
+      err
+        ?
+        reject(err) :
+        resolve(res1);
+    });
 
-               }
-               err
-                  ? reject(err)
-                  : resolve(res1);
-             }).limit(5);
-           }
-           if (flag != 1) {
-             flag = 0;
-             Ads.find({
-               brand: {
-                 $in: [regexp1, regexp2]
-               }
-             }, function(err, data) {
-               if (data.length != 0) {
-                 res1 = {
-                   Route: 2,
-                   data: data
-                 };
-               }
-               err
-                  ? reject(err)
-                  : resolve(res1);
-             }).limit(5);
-           }
 
 
-         });
-       };
+      });
+    };
 
-       //Step 2: async promise handler
-       var callMyPromise = async () => {
+    //Step 2: async promise handler
+    var callMyPromise = async () => {
 
-          var result = await (myPromise());
-          //anything here is executed after result is resolved
-          return result;
-       };
+      var result = await (myPromise());
+      //anything here is executed after result is resolved
+      return result;
+    };
 
-       //Step 3: make the call
-       callMyPromise().then(function(result) {
-          res.json(result);
-       });
+    //Step 3: make the call
+    callMyPromise().then(function(result) {
+      res.json(result);
+    });
 
-   } catch (e) {
-     next(e)
-   }
+  } catch (e) {
+    next(e)
+  }
 });
 
 searchRouter.post('/search/:q', function(req, res, next) {
@@ -110,70 +139,72 @@ searchRouter.post('/search/:q', function(req, res, next) {
   let res1 = {};
 
   try {
-       //Step 1: declare promise
+    //Step 1: declare promise
 
-       var myPromise = () => {
-         return new Promise((resolve, reject) => {
-           flag = 0;
-           if (flag == 0) {
-             Ads.find({
-               sub_category: {
-                 $in: [regexp1, regexp2]
-               }
-             }, function(err, data) {
-               if (data.length != 0) {
-                 res1 = {
-                   Route: 1,
-                   data: data
-                 };
+    var myPromise = () => {
+      return new Promise((resolve, reject) => {
+        flag = 0;
+        if (flag == 0) {
+          Ads.find({
+            sub_category: {
+              $in: [regexp1, regexp2]
+            }
+          }, function(err, data) {
+            if (data.length != 0) {
+              res1 = {
+                Route: 1,
+                data: data
+              };
 
-                 flag = 1;
+              flag = 1;
 
-               }
-               err
-                  ? reject(err)
-                  : resolve(res1);
-             }).limit(5);
-           }
-           if (flag != 1) {
-             flag = 0;
-             Ads.find({
-               brand: {
-                 $in: [regexp1, regexp2]
-               }
-             }, function(err, data) {
-               if (data.length != 0) {
-                 res1 = {
-                   Route: 2,
-                   data: data
-                 };
-               }
-               err
-                  ? reject(err)
-                  : resolve(res1);
-             }).limit(5);
-           }
+            }
+            err
+              ?
+              reject(err) :
+              resolve(res1);
+          }).limit(5);
+        }
+        if (flag != 1) {
+          flag = 0;
+          Ads.find({
+            brand: {
+              $in: [regexp1, regexp2]
+            }
+          }, function(err, data) {
+            if (data.length != 0) {
+              res1 = {
+                Route: 2,
+                data: data
+              };
+            }
+            err
+              ?
+              reject(err) :
+              resolve(res1);
+          }).limit(5);
+        }
 
 
-         });
-       };
+      });
+    };
 
-       //Step 2: async promise handler
-       var callMyPromise = async () => {
+    //Step 2: async promise handler
+    var callMyPromise = async () => {
 
-          var result = await (myPromise());
-          //anything here is executed after result is resolved
-          return result;
-       };
+      var result = await (myPromise());
+      //anything here is executed after result is resolved
+      return result;
+    };
 
-       //Step 3: make the call
-       callMyPromise().then(function(result) {
-          res.json(result);
-       });
+    //Step 3: make the call
+    callMyPromise().then(function(result) {
+      res.json(result);
+    });
 
-   } catch (e) {
-     next(e)
-   }
+  } catch (e) {
+    next(e)
+  }
 
   //for searching in sub-category
   // if (flag == 0) {
