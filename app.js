6ecc142fa2aa2f5d4e8c7afe71ada3e587eba029
Tanveesh Singh Chaudhery <tanveeshs@gmail.com>
2020-08-14 22:58:54 +0530

    Cloud

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const wishlist = require('./routes/wishlist');
 const offers = require('./routes/offers');
 const adminRouter = require('./routes/admin')
 // const smsverify = require('./routes/sms');
-var forceSsl = require('express-force-ssl');
 
 const PORT = process.env.PORT || 8080;
 mongoose.connect(configDB.url, {
@@ -117,10 +116,9 @@ app.get('/CloudTest',(req,res)=>{
 
 //for app engine
 
-// app.use(forceSsl);
-// app.listen(PORT, () => {
-//     console.log(`Server listening on port ${PORT}...`);
-// });
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}...`);
+});
 
 
 //for compute engine
@@ -135,6 +133,6 @@ app.get('/CloudTest',(req,res)=>{
 
 //local test
 
-var http_server = http.createServer(app).listen(3000, function(err){
-    console.log("Node.js Express HTTPS Server Listening on Port 300to app0");
-});
+// var http_server = http.createServer(app).listen(3000, function(err){
+//     console.log("Node.js Express HTTPS Server Listening on Port 300to app0");
+// });
