9dd1f89c710e82d307c7a697e43a0293d882db7a
Tanveesh Singh Chaudhery <tanveeshs@gmail.com>
2020-08-30 00:12:54 +0530

    Category

--- routes/searchRoute.js
+++ routes/searchRoute.js
@@ -31,100 +31,128 @@ const async = require('async')
 //   }).limit(5);
 // });
 searchRouter.post('/main', function(req, res, next) {
-  var q = req.body.searchInput;
-  const regexp1 = new RegExp(`^${q}`, 'i'); // for finding it as beginning of the first word
-  const regexp2 = new RegExp(`\\s${q}`, 'i'); // for finding as beginning of a middle word
-  var query = {}
-  query.$and = []
-  if(typeof(req.body.searchInput)!='undefined') {
-    let orArray = [];
-    orArray.push({sub_category: {
-        $in: [regexp1, regexp2]
-      }})
-    orArray.push({title: {
-        $in: [regexp1, regexp2]
-      }});
-    orArray.push({brand: {
-        $in: [regexp1, regexp2]
-      }});
-    orArray.push({category: {
-        $in: [regexp1, regexp2]
-      }});
-    orArray.push({description: {
-        $in: [regexp1, regexp2]
-      }});
-    query.$and.push({$or:orArray})
-  }
-  if(typeof(req.body.locality)!=='undefined') {
-    query.$and.push({deliverableAreas: req.body.locality})
-  }
-  if(typeof(req.body.category)!=='undefined') {
-    query.$and.push({category: req.body.category})
-  }
-  if(typeof(req.body.sub_category) !== 'undefined') {
-    query.$and.push({category: req.body.sub_category})
+  if(req.body.isAjax==='1') {
+    var q = req.body.searchInput;
+    console.log(req.body)
+    const regexp1 = new RegExp(`^${q}`, 'i'); // for finding it as beginning of the first word
+    const regexp2 = new RegExp(`\\s${q}`, 'i'); // for finding as beginning of a middle word
+    var query = {}
+    query.$and = []
+    if (typeof (req.body.searchInput) != 'undefined') {
+      let orArray = [];
+      orArray.push({
+        sub_category: {
+          $in: [regexp1, regexp2]
+        }
+      })
+      orArray.push({
+        title: {
+          $in: [regexp1, regexp2]
+        }
+      });
+      orArray.push({
+        brand: {
+          $in: [regexp1, regexp2]
+        }
+      });
+      orArray.push({
+        category: {
+          $in: [regexp1, regexp2]
+        }
+      });
+      orArray.push({
+        description: {
+          $in: [regexp1, regexp2]
+        }
+      });
+      query.$and.push({$or: orArray})
+    }
+    if (typeof (req.body.locality) !== 'undefined') {
+      query.$and.push({deliverableAreas: req.body.locality})
+    }
+    if (typeof (req.body.category) !== 'undefined') {
+      query.$and.push({category: req.body.category})
+    }
+    if (typeof (req.body.sub_category) !== 'undefined') {
+      query.$and.push({category: req.body.sub_category})
+
+    }
+    if (typeof (req.body.lower_price) !== 'undefined') {
+      query.$and.push({price: {$gte: parseFloat(req.body.lower_price)}})
+    }
+    if (typeof (req.body.upper_price) !== 'undefined') {
+      query.$and.push({price: {$lte: parseFloat(req.body.upper_price)}})
+    }
+    query.$and.push({approved: true})
+    query.$and.push({isActive: true})
+    query.$and.push({'rejected.val': false})
+    //Low to high
+    let page = 0 || req.body.page;
+    if (parseInt(req.body.sorting) === 1) {
+
+      Ads.find(query, function (err, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          if (req.body.isAjax === '1') {
+            //Render the page
+            console.log(result)
+            res.json(result);
+          } else {
+            res.render('category.ejs', {
+              body: req.body,
+              ads: result
+            })
+          }
+        }
+      }).sort({price: 1}).limit(5).skip(5 * page)
+    }
+    //High To low
+    if (parseInt(req.body.sorting) === 2) {
+      Ads.find(query, function (err, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          if (req.body.isAjax === '1') {
+            //Render the page
+            res.json(result);
+          } else {
+            res.render('category.ejs', {
+              ads: result
+            })
+          }
+        }
+      }).sort({price: -1}).limit(5).skip(5 * page)
+    }
+    //Most Recent
+    if (parseInt(req.body.sorting) === 3) {
+      Ads.find(query, function (err, result) {
+        if (err) {
+          console.log(err);
+        } else {
+          if (req.body.isAjax === '1') {
+            //Render the page
+            res.json(result);
+          } else {
+            res.render('category.ejs', {
+              ads: result
+            })
+          }
+        }
+      }).sort({date_posted: -1}).limit(5).skip(5 * page)
+    }
+
+  }else {
+    return res.render('category.ejs',{
+      body:req.body,
+      user:req.user
+    })
 
   }
-  if(typeof(req.body.lower_price) !== 'undefined') {
-    query.$and.push({price:{$gte:parseFloat(req.body.lower_price)}})
-  }
-  if(typeof(req.body.upper_price) !== 'undefined') {
-    query.$and.push({price:{$lte:parseFloat(req.body.upper_price)}})
-  }
-  query.$and.push({approved:true})
-  query.$and.push({isActive:true})
-  query.$and.push({'rejected.val':false})
-  //Low to high
-  let page = 0 || req.body.page;
-  if(parseInt(req.body.sorting) === 1){
 
-    Ads.find(query, function(err,result){
-      if(err){
-        console.log(err);
-      }else {
-        if(typeof(req.body.isAjax) !== 'undefined'){
-          //Render the page
-          res.json(result);
-        }
-        else {
-          res.json(result);
-        }
-      }
-    }).sort({price:1}).limit(9).skip(9*page)
-  }
-  //High To low
-  if(parseInt(req.body.sorting) === 2){
-    Ads.find(query, function(err,result){
-      if(err){
-        console.log(err);
-      }else {
-        if(typeof(req.body.isAjax) !== 'undefined'){
-          //Render the page
-          res.json(result);
-        }
-        else {
-          res.json(result);
-        }
-      }
-    }).sort({price:-1}).limit(9).skip(9*page)
-  }
-  //Most Recent
-  if(parseInt(req.body.sorting) === 3){
-    Ads.find(query, function(err,result){
-      if(err){
-        console.log(err);
-      }else {
-        if(typeof(req.body.isAjax) !== 'undefined'){
-          //Render the page
-          res.json(result);
-        }
-        else {
-          res.json(result);
-        }
-      }
-    }).sort({date_posted:-1}).limit(9).skip(9*page)
-  }
 });
+
+
 
 searchRouter.post('/search/:q', (req, res) => {
   var q = req.params.q;
