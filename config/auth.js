62a4575b113f8d321e87800c23d15cd93b9ebbc1
Tanveesh Singh Chaudhery <tanveeshs@gmail.com>
2020-08-29 20:02:05 +0530

    Facebook Login

--- config/auth.js
+++ config/auth.js
@@ -9,6 +9,6 @@ module.exports = {
     'clientSecret': process.env.facebook_client_secret,
     'callbackURL': 'https://bechdaal.tech/auth/facebook/callback',
     'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
-    'profileFields': ['id']
+    'profileFields': ['id','displayName','emails']
   },
 };
